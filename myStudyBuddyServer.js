9fc00c07903f80faf07b207d5c22199e01727aa8
LAPTOP-06L6KPT9\17ral <rea63@drexel.edu>
2019-08-27 14:16:59 -0400

    server.js update

--- myStudyBuddyServer.js
+++ myStudyBuddyServer.js
@@ -1,9 +1,20 @@
-const express = require('express');
+var express = require("express");
+var request = require("request");
+var mysql = require("mysql");
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "rea63drexel",
+	database: "transcript"
+});
 
 var app = express();
+app.use(express.static(__dirname));
 
 app.get('/',function(req,res) {
     res.json({'ans':1});
 });
 
-app.listen(8080);
+app.listen(8080, function() {
+	console.log('Server Started...');
+});
