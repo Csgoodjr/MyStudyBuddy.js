b4af8434ab7e5c0f68a6bbb69f642f4891d49ba1
LAPTOP-06L6KPT9\17ral <rea63@drexel.edu>
2019-08-27 15:24:08 -0400

    started JS ajax request for adding user

--- public/myStudyBuddy.js
+++ public/myStudyBuddy.js
@@ -19,3 +19,31 @@ function view_projects() {
 function view_chats() {
     console.log("Chats");
 }
+
+function add_user() {
+	conscole.log("New User...");
+	
+	var email = document.getElementById("email").value;
+	var firstName = document.getElementById("firstName").value;
+	var id = document.getElementById("id").value;
+	var lastName = document.getElementById("lastName").value;
+	var password = document.getElementById("password").value;
+	var username = document.getElementById("username").value;
+	
+	var URL = "http://localhost:8080/add?" + "email=" + email + "&firstName=" + firstName +
+		"&id=" + id + "&lastName=" + lastName + "&password=" + password + "&username=" + username;
+	
+	$.ajax({
+		type: "GET",
+		url: URL,
+		contentType: "application/json; charset=utf-8",
+		data: "{}",
+		dataType: "html",
+		success: function(msg) {
+			//$("#content").html(msg);
+		},
+		error: function(xhr, ajaxOptions, thrownError) {
+			//$("#content").html("<p>Error fetching " + URL + "</p>");
+		}
+	});
+}
